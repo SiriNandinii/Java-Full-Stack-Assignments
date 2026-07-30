2a1844c7eabc7d71697bd566aa37becf44394090
SiriNandinii <149805234+SiriNandinii@users.noreply.github.com>
2026-07-30 15:47:26 +0530

    Add files via upload

---
+++ Java/oops2/TollBooth.java
@@ -0,0 +1,14 @@
+package day4;
+
+/**
+* Author : sirin
+* Date : Jul 17, 2026
+* Time : 10:15:03 AM
+* Email : sirinandini.a@gmail.com
+*/
+
+public interface TollBooth {
+    void calculateToll(Truck truck);
+    void displayData();
+    void reset();
+}
